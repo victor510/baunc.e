e71b001ffff592e62ded034f580b90d09047dddb
wyClif <wyclifgachara@gmail.com>
2022-01-26 00:19:05 +0300

    This wasn't supposed to be a major commit nothing much has changed. I just wanted to move on to the next section of creating a gallery

--- Assets/Js/gsapAnimations.js
+++ Assets/Js/gsapAnimations.js
@@ -6,19 +6,16 @@ gsap.from('.hero h1', {
     transform: 'translateY(50px)',
     ease: 'Expo.2'
 })
+gsap.from('.hero p', {
+    duration: 1.4,
+    opacity: .4,
+    transform: 'translateY(70px)',
+    ease: 'Expo.2'
+})
 gsap.from('ul li', {
     delay: .4,
     duration: 1.8,
     opacity: 0,
     stagger: .4,
     transform: 'translateY(-2em)'
-})
-
-const feature1 = document.querySelector('.feature_1_img');
-
-feature1.addEventListener('mouseenter', ()=> {
-    gsap.to('.overlay', {
-        duration: .8,
-        opacity: 1
-    })
 })
