97e3b6a70183fc67440101ca1aa1bb9b0ea32f28
wyClif <wyclifgachara@gmail.com>
2022-01-25 23:57:10 +0300

    I have completed basic GSAP animations and hover efx 👍

--- Assets/Js/gsapAnimations.js
+++ Assets/Js/gsapAnimations.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.from('.hero h1', {
     duration: 1.8,
     opacity: 0,
@@ -10,4 +12,13 @@ gsap.from('ul li', {
     opacity: 0,
     stagger: .4,
     transform: 'translateY(-2em)'
+})
+
+const feature1 = document.querySelector('.feature_1_img');
+
+feature1.addEventListener('mouseenter', ()=> {
+    gsap.to('.overlay', {
+        duration: .8,
+        opacity: 1
+    })
 })
