2e7b53741c32c53eff0b3f9a73d2d89f1ca2c637
wyClif <wyclifgachara@gmail.com>
2022-02-04 17:19:47 +0300

    The header is now responsive to scrolling thanks to Js intersection Observer API

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -2,6 +2,9 @@ const menuBtn = document.querySelector('.menu_btn')
 const closeBtn = document.querySelector('.close_btn')
 const nav = document.querySelector('nav')
 
+const header = document.querySelector('header');
+const sectionOne = document.querySelector('.hero');
+
 menuBtn.addEventListener('click', ()=> {
     nav.classList.add('open')
 })
@@ -9,3 +12,22 @@ menuBtn.addEventListener('click', ()=> {
 closeBtn.addEventListener('click', ()=> {
     nav.classList.remove('open')
 })
+
+const sectionOneOptions = {
+    rootMargin: '-250px 0px 0px 0px'
+ }
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries, sectionOneObserver) 
+{
+    entries.forEach(entry => {
+
+        if(!entry.isIntersecting) {
+            header.classList.add('scrolled')
+        } else {
+            header.classList.remove('scrolled')
+        }
+    })
+}, sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
